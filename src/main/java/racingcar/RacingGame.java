a39ada053d525c8bdb91fae87f5b72f81c31ae37
dino.shin <troas96@naver.com>
2024-03-26 15:19:21 +0900

    test: 게임 진행 후 우승자 반환 테스트 작성
    Co-authored-by: zeroberry <98147817+zeroberry@users.noreply.github.com>

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -2,6 +2,7 @@ package racingcar;
 
 import racingcar.model.NumberGenerator;
 import racingcar.model.Cars;
+import racingcar.model.StepCount;
 
 import java.util.List;
 
@@ -9,16 +10,22 @@ public class RacingGame {
 
     private final Cars cars;
     private final NumberGenerator numberGenerator;
+    private StepCount stepCount;
 
-    public RacingGame(final String inputNames, final NumberGenerator numberGenerator) {
+    public RacingGame(final String inputNames, final NumberGenerator numberGenerator, final int inputCount) {
         this.cars = new Cars(inputNames);
         this.numberGenerator = numberGenerator;
+        this.stepCount = new StepCount(inputCount);
     }
 
     public void play() {
+        stepCount = stepCount.decrease();
         cars.step(numberGenerator);
     }
 
+    public boolean isEnd() {
+        return stepCount.isZero();
+    }
 
     public Cars getCars() {
         return cars;
