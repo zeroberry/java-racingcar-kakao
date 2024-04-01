2d10b98cdda8ec6ca9d1699fec4e3adaebe5bdc3
zeroberry <98147817+zeroberry@users.noreply.github.com>
2024-04-01 10:14:01 +0900

    test: RacingGameTest 라운드 진행 테스트 추가

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -8,9 +8,24 @@ import racingcar.model.RacingGame;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RacingGameTest {
+
+    @DisplayName("의도한 횟수만큼 라운드가 진행된다.")
+    @Test
+    void playTest() {
+        // given
+        final RacingGame racingGame = new RacingGame("lucas", new CustomCarMoveStrategy(List.of(false, false, false)), 3);
+
+        // when
+        racingGame.play();
+        racingGame.play();
+        racingGame.play();
+
+        // then
+        assertThat(racingGame.isEnd()).isTrue();
+    }
 
     @DisplayName("게임을 완료한 후 우승자를 반환한다.")
     @Test
