ecdca96c7498301a77050d7b1a7a02540204f151
zeroberry <98147817+zeroberry@users.noreply.github.com>
2024-04-01 09:59:18 +0900

    style: reformat CustomCarMoveStrategy

--- src/test/java/racingcar/model/CustomCarMoveStrategy.java
+++ src/test/java/racingcar/model/CustomCarMoveStrategy.java
@@ -3,7 +3,7 @@ package racingcar.model;
 import java.util.LinkedList;
 import java.util.List;
 
-public class CustomCarMoveStrategy implements CarMoveStrategy{
+public class CustomCarMoveStrategy implements CarMoveStrategy {
 
     private final LinkedList<Boolean> moves;
 
